3b140d6642083a84f31f6f76739b568de89815f4
lynnagidza <lynnagidza@gmail.com>
2023-06-29 10:23:22 +0300

    task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module contains a function that returns a list of lists of integers"""
+
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's triangle of size n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(row)
+
+    return triangle
