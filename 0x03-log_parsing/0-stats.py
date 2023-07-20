1c4f8c0c339045e87ace54a5b1c2451e74f2330c
lynnagidza <lynnagidza@gmail.com>
2023-07-20 15:47:27 +0300

    correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,53 @@
- #!/usr/bin/python3
+#!/usr/bin/env python3
 """Log Parsing"""
+
 import sys
 
+def print_stats(total_file_size, status_codes):
+    """Prints the stats"""
+    print("File size:", total_file_size)
+    for code in sorted(status_codes.keys()):
+        print(f"{code}: {status_codes[code]}")
 
-def print_stats(status_codes, file_size):
-    """Prints the statistics"""
-    print(f"File size: {file_size}")
-    for key, value in sorted(status_codes.items()):
-        if value != 0:
-            print(f"{key}: {value}")
+def main():
+    """Main function"""
+    total_file_size = 0
+    status_codes = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+    }
 
+    try:
+        for idx, line in enumerate(sys.stdin, start=1):
+            line = line.strip()
+            parts = line.split()
+
+            if len(parts) != 7:
+                continue
+
+            _, _, _, status_code_str, file_size_str = parts
+
+            try:
+                status_code = int(status_code_str)
+                file_size = int(file_size_str)
+            except ValueError:
+                continue
+
+            total_file_size += file_size
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+
+            if idx % 10 == 0:
+                print_stats(total_file_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(total_file_size, status_codes)
 
 if __name__ == "__main__":
-    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                    '403': 0, '404': 0, '405': 0, '500': 0}
-    file_size = 0
-    counter = 0
-    try:
-        for line in sys.stdin:
-            args = line.split()
-            if len(args) > 2:
-                file_size += int(args[-1])
-                if args[-2] in status_codes:
-                    status_codes[args[-2]] += 1
-                counter += 1
-                if counter % 10 == 0:
-                    print_stats(status_codes, file_size)
-        print_stats(status_codes, file_size)
-    except KeyboardInterrupt:
-        print_stats(status_codes, file_size)
-        raise
+    main()
